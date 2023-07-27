1c138f6556c2ed65f3dde1dc30413628eb447fbc
derekcheungsa <122410431+derekcheungsa@users.noreply.github.com>
2023-07-26 20:35:59 -0400

    updated

--- app.py
+++ app.py
@@ -1,35 +1,17 @@
+
 import chainlit as cl
+import requests
 
-# This is a simple example on how to prompt a user to confirm or reject an action.
+API_URL = "https://flowiseai-docker.onrender.com/api/v1/prediction/a396d64e-8a11-4467-88dc-df31507ce447"
 
-
-@cl.action_callback("confirm_action")
-async def on_action(action: cl.Action):
-    if action.value == "ok":
-        content = "Confirmed!"
-    elif action.value == "not_ok":
-        content = "Rejected!"
-    else:
-        await cl.ErrorMessage(content="Invalid action").send()
-        return
-
-    actions = cl.user_session.get("actions")
-    if actions:
-        for action in actions:
-            await action.remove()
-        cl.user_session.set("actions", None)
-
-    await cl.Message(content=content).send()
-
-
-@cl.on_chat_start
-async def start():
-    approve_action = cl.Action(name="confirm_action", value="ok", label="Confirm")
-    reject_action = cl.Action(name="confirm_action", value="not_ok", label="Reject")
-    actions = [approve_action, reject_action]
-    cl.user_session.set("actions", actions)
-
+@cl.on_message
+async def main(message: str):
+    # Your custom logic goes here...
+   
+    response = requests.post(API_URL, 
+                             json={"question": message})
+    
+    # Send a response back to the user
     await cl.Message(
-        content="Test message",
-        actions=actions,
+        content=f"Received: {response.text}",
     ).send()
